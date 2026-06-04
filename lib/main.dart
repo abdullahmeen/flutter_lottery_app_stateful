a23336acce928cd0f2ff08ea16e86d04f0f18ba3
abdullahmeen <abdullahamen321@gmail.com>
2026-06-04 15:34:59 +0500

    imlemented how the boile code works for button tapped shows how many times button is pressed statefull

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,54 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp( MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  int x = 0;
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    print('rebuild');
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-            body: SafeArea(child: Text('data')),
-          )
+
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.blue,
+            title: Text('Lottery App UI'),
+          ),
+          body: SafeArea(
+              child: Center(
+                child: Text(x.toString(),
+                  style: TextStyle(
+                    fontSize: 50,
+                  ),),
+              )
+          ),
+          floatingActionButton: FloatingActionButton(
+              child: Icon(Icons.add),
+              onPressed: ()
+              {
+                x++;
+                setState(() {
+
+                });
+                print(x);
+              }
+
+          ),
+        )
 
     );
   }
 }
+
